c78db73aee048d4fd7499ddc831d518f561b355a
Dhanny Adhi Pramana <dhanny.pramana@nostratech.com>
2025-03-24 19:24:03 +0700

    feat(sidebar): add sidebar base component

--- tailwind.config.js
+++ tailwind.config.js
@@ -138,6 +138,16 @@ export default {
                     muted: '#374151',
                     contrast: '#F9FAFB',
                 },
+                sidebar: {
+                    'DEFAULT': 'hsl(var(--sidebar-background))',
+                    'foreground': 'hsl(var(--sidebar-foreground))',
+                    'primary': 'hsl(var(--sidebar-primary))',
+                    'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+                    'accent': 'hsl(var(--sidebar-accent))',
+                    'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+                    'border': 'hsl(var(--sidebar-border))',
+                    'ring': 'hsl(var(--sidebar-ring))',
+                },
             },
             boxShadow: {
                 smooth: '0px 5px 40px rgba(10, 10, 10, 0.1)',
