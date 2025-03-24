46682b42909e38e6016f6f9e8a3fb1c60e93f69a
Dhanny Adhi Pramana <dhanny.pramana@nostratech.com>
2025-03-24 13:48:21 +0700

    feat(tooltip): add tooltip component

---
+++ components/base/VTooltip/VTooltip.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { TooltipRootEmits, TooltipRootProps } from 'reka-ui';
+import { TooltipRoot, useForwardPropsEmits } from 'reka-ui';
+
+const props = defineProps<TooltipRootProps>();
+const emits = defineEmits<TooltipRootEmits>();
+
+const forwarded = useForwardPropsEmits(props, emits);
+</script>
+
+<template>
+    <VTooltipProvider>
+        <TooltipRoot v-bind="forwarded">
+            <VTooltipTrigger as-child>
+                <slot name="trigger" />
+            </VTooltipTrigger>
+            <VTooltipContent>
+                <slot name="content" />
+            </VTooltipContent>
+        </TooltipRoot>
+    </VTooltipProvider>
+</template>
