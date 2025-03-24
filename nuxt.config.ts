545b0f12734d92c00a63b699479ef3c0d0fb36a4
Dhanny Adhi Pramana <dhanny.pramana@nostratech.com>
2025-03-24 11:06:45 +0700

    chore(deps): add vue motion

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,6 +18,7 @@ export default defineNuxtConfig({
         'pinia-plugin-persistedstate/nuxt',
         '@nuxtjs/tailwindcss',
         'shadcn-nuxt',
+        'motion-v/nuxt',
     ],
 
     eslint: {
