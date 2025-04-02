d518bccf0af4becc327ebd577d6e43a51ae55af1
Dhanny Adhi Pramana <dhanny.pramana@nostratech.com>
2025-04-02 09:33:47 +0700

    feat(base): setup general example

---
+++ constants/GeneralConstant.ts
@@ -0,0 +1,10 @@
+export const CONTENT_STATUS = {
+    DRAFT: 'DRAFT',
+    PUBLISHED: 'PUBLISHED',
+    ARCHIVED: 'ARCHIVED',
+} as const;
+
+export const SORT_DIRECTION = {
+    ASCENDING: 'asc',
+    DESCENDING: 'desc',
+} as const;
